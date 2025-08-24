cebd151581197ef43769c0a9d9d6cac374b5bed1
Ameer Deen <writeameer@gmail.com>
2025-08-24 08:49:49 +0400

    Updated test

--- test/HCLog.Net.Tests/UnitTest1.cs
+++ test/HCLog.Net.Tests/UnitTest1.cs
@@ -1,10 +1,13 @@
 ﻿namespace HCLog.Net.Tests;
+using HCLog.Net;
+
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
+        var logger = new HCLogger("MyApp");
+        logger.Info("Application started");
     }
 }
